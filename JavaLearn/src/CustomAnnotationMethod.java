077965108cb1ac16ed77a0cef5f31de7a9096cd7
Serhiy Brytskyy <sbrytskyy@gmail.com>
2017-04-25 16:37:22 -0700

    Annotations

---
+++ JavaLearn/src/CustomAnnotationMethod.java
@@ -0,0 +1,16 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CustomAnnotationMethod {
+
+	public String author() default "danibuiza";
+
+	public String date();
+
+	public String description();
+
+}
