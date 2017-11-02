516bb89c9e4b445a4412a1b713e3f5c554519cf7
Serhiy Brytskyy <sbrytskyy@gmail.com>
2017-11-01 17:28:48 -0700

    MTH tests

---
+++ JavaLearn/src/com/sb/test/deadlock/DeadLockMain.java
@@ -0,0 +1,42 @@
+package com.sb.test.deadlock;
+
+public class DeadLockMain {
+
+	static class Friend {
+		private final String name;
+
+		public Friend(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public synchronized void bow(Friend f) {
+			//System.out.format("%s: %s" + "  has bowed to me!%n", this.name, f.getName());
+			System.out.println(this.name+ ": " + f.getName() + "  has bowed to me!%n");
+			f.bowBack(this);
+		}
+
+		public synchronized void bowBack(Friend f) {
+			System.out.format("%s: %s" + " has bowed back to me!%n", this.name, f.getName());
+		}
+	}
+
+	public static void main(String[] args) {
+		final Friend a = new Friend("A");
+		final Friend b = new Friend("B");
+
+		new Thread(new Runnable() {
+			public void run() {
+				a.bow(b);
+			}
+		}).start();
+		new Thread(new Runnable() {
+			public void run() {
+				b.bow(a);
+			}
+		}).start();
+	}
+}
