85329d559f1f4e846eb7f464f7280913a381c10b
Serhiy Brytskyy <sbrytskyy@gmail.com>
2019-04-09 11:20:56 -0700

    Fixes

--- TestAutoValue/src/main/java/com/sb/test/auto/User.java
+++ TestAutoValue/src/main/java/com/sb/test/auto/User.java
@@ -2,10 +2,7 @@ package com.sb.test.auto;
 
 import com.google.auto.value.AutoValue;
 
-import com.google.common.annotations.VisibleForTesting;
-
 @AutoValue
-@VisibleForTesting
 abstract class User {
 	static User create(Long id, String name) {
 		return new AutoValue_User(id, name);
