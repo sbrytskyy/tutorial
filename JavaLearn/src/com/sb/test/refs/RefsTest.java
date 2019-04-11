d9cdbec736b03e3999c826b985dcc1fefd8acba0
Serhiy Brytskyy <sbrytskyy@gmail.com>
2019-04-11 11:25:38 -0700

    is enqueued

---
+++ JavaLearn/src/com/sb/test/refs/RefsTest.java
@@ -0,0 +1,109 @@
+package com.sb.test.refs;
+
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+
+public class RefsTest implements Runnable {
+
+	class Counter {
+		private int count;
+
+		int getCount() {
+			return count;
+		}
+
+		Counter(int count) {
+			this.count = count;
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			testWeak();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			testSoft();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			testWeakHashMap();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void testWeak() {
+		System.out.println("Test WeakReference");
+
+		Counter counter = new Counter(10);
+
+		WeakReference<Counter> weakCounter = new WeakReference<Counter>(counter);
+		System.out.println("weakCounter: " + weakCounter.get().getCount() + ", " + weakCounter.isEnqueued());
+
+		counter = null;
+
+		System.out.println("weakCounter: " + weakCounter.get().getCount() + ", " + weakCounter.isEnqueued());
+
+		System.gc();
+
+		System.out.println("weakCounter: " + weakCounter.get().getCount() + ", " + weakCounter.isEnqueued());
+
+	}
+
+	private void testSoft() {
+		System.out.println("Test SoftReference");
+
+		Counter counter = new Counter(10);
+
+		SoftReference<Counter> softCounter = new SoftReference<Counter>(counter);
+		System.out.println("softCounter: " + softCounter.get().getCount());
+
+		counter = null;
+
+		System.out.println("softCounter: " + softCounter.get().getCount() + ", " + softCounter.isEnqueued());
+
+		System.gc();
+
+		System.out.println("softCounter: " + softCounter.get().getCount() + ", " + softCounter.isEnqueued());
+
+		System.gc();
+
+		System.out.println("softCounter: " + softCounter.get().getCount() + ", " + softCounter.isEnqueued());
+
+		System.gc();
+
+		System.out.println("softCounter: " + softCounter.get().getCount() + ", " + softCounter.isEnqueued());
+	}
+
+	private void testWeakHashMap() {
+		System.out.println("Test WeakReference");
+
+		Counter counter = new Counter(10);
+
+		WeakHashMap<Counter, Integer> whm = new WeakHashMap<>();
+		whm.put(counter, counter.getCount());
+		
+		System.out.println("weakCounter: " + whm.get(counter));
+
+		counter = null;
+
+		System.out.println("weakCounter: " + whm.get(counter));
+
+		System.gc();
+
+		System.out.println("weakCounter: " + whm.get(counter));
+
+	}
+
+	public static void main(String[] args) {
+		RefsTest t = new RefsTest();
+		t.run();
+	}
+}
