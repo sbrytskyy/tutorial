a114ff6d2d4e566b2cf360a4f5c246abb5874c26
Serhiy Brytskyy <sbrytskyy@gmail.com>
2020-01-08 18:38:38 -0800

    set pdf password

--- PdfPassword/src/com/sb/test/pdf/Main.java
+++ PdfPassword/src/com/sb/test/pdf/Main.java
@@ -12,8 +12,12 @@ import java.io.*;
 
 public class Main {
 
-	public static final String DEST = "outputWithPwdTest.pdf";
-	public static final String SRC = "inputTest.pdf";
+	private static final String PWD_OWNER = "d8erEAgncGKYJaKD";
+	
+	private static final String PWD_USER = "d8erEAgncGKYJaKD";
+	
+	public static final String DEST = "Decl_of_Fed_ Empl_NK_enc.pdf";
+	public static final String SRC = "Decl_of_Fed_ Empl_NK.pdf";
 
 	public static void main(String[] args) {
 		try {
@@ -25,10 +29,16 @@ public class Main {
 	}
 
 	private static void setPdfPwd() throws IOException {
-		PdfReader reader = new PdfReader(SRC, new ReaderProperties().setPassword("World".getBytes()));
+		PdfReader reader = new PdfReader(SRC, new ReaderProperties().setPassword(PWD_OWNER.getBytes()));
 		PdfWriter writer = new PdfWriter(DEST,
-				new WriterProperties().setStandardEncryption("Hello".getBytes(), "World".getBytes(),
-						EncryptionConstants.ALLOW_PRINTING,
+				new WriterProperties().setStandardEncryption(PWD_USER.getBytes(), PWD_OWNER.getBytes(),
+						EncryptionConstants.ALLOW_PRINTING | 
+						EncryptionConstants.ALLOW_COPY | 
+						EncryptionConstants.ALLOW_MODIFY_ANNOTATIONS | 
+						EncryptionConstants.ALLOW_FILL_IN | 
+						EncryptionConstants.ALLOW_SCREENREADERS | 
+//						EncryptionConstants.ALLOW_ASSEMBLY | 
+						EncryptionConstants.ALLOW_MODIFY_CONTENTS,
 						EncryptionConstants.ENCRYPTION_AES_128 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA));
 		PdfDocument pdfDoc = new PdfDocument(reader, writer);
 		pdfDoc.close();
