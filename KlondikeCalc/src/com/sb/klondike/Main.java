d1012b730cf3e3037e9691f82fcdad3f060dba42
Serhiy Brytskyy <sbrytskyy@gmail.com>
2020-04-24 23:15:57 -0700

    Fixed prettyPrintProduct. Added getTotalIngredientCounts method.

--- KlondikeCalc/src/com/sb/klondike/Main.java
+++ KlondikeCalc/src/com/sb/klondike/Main.java
@@ -10,11 +10,24 @@ public class Main {
 	private void run() {
 		Water p1 = BarnFactory.water();
 		Product.prettyPrintProduct(p1);
+		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p1));
+
+		System.out.println();
 
 		Nails p2 = SmithyFactory.nails();
 		Product.prettyPrintProduct(p2);
+		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p2));
 
-		Chains p3 = SmithyFactory.chains();
+		System.out.println();
+
+		Pipes p3 = SmithyFactory.pipes();
 		Product.prettyPrintProduct(p3);
+		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p3));
+
+		System.out.println();
+
+		Chains p4 = SmithyFactory.chains();
+		Product.prettyPrintProduct(p4);
+		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p4));
 	}
 }
--- KlondikeCalc/src/com/sb/klondike/Product.java
+++ KlondikeCalc/src/com/sb/klondike/Product.java
@@ -2,7 +2,10 @@ package com.sb.klondike;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
 
 public abstract class Product extends Ingredient {
 
@@ -31,10 +34,38 @@ public abstract class Product extends Ingredient {
 		return sb.toString();
 	}
 
+	public static Map<Ingredient, Integer> getTotalIngredientCounts(Product p) {
+		Map<Ingredient, Integer> totalIngredientCounts = new HashMap<>();
+
+		Queue<Map.Entry<Ingredient, Integer>> q = new LinkedList<>();
+		for (Map.Entry<Ingredient, Integer> entry : p.ingredientCounts.entrySet()) {
+			q.offer(entry);
+		}
+
+		while (!q.isEmpty()) {
+			Entry<Ingredient, Integer> entry = q.poll();
+			Ingredient ingredient = entry.getKey();
+			Integer count = entry.getValue();
+			if (ingredient instanceof Product) {
+				Product subProduct = (Product) ingredient;
+				for (Map.Entry<Ingredient, Integer> subEntry : subProduct.getIngredientCounts().entrySet()) {
+					Map.Entry<Ingredient, Integer> newSubEntry = Map.entry(subEntry.getKey(),
+							subEntry.getValue() * count);
+					q.offer(newSubEntry);
+				}
+			} else {
+				Integer currentCount = totalIngredientCounts.getOrDefault(ingredient, 0);
+				totalIngredientCounts.put(ingredient, currentCount + count);
+			}
+		}
+
+		return Collections.unmodifiableMap(totalIngredientCounts);
+	}
+
 	public static void prettyPrintProduct(Product p) {
-		System.out.println("-*** PrettyPrintProduct ***-");
+		System.out.println("-*** " + p.getName() + " ***-");
 		prettyPrintProduct(p, "");
-		System.out.println("-*** PrettyPrintProduct ***-");
+		System.out.println("-***************************-");
 	}
 
 	private static void prettyPrintProduct(Product p, String prefix) {
@@ -44,8 +75,9 @@ public abstract class Product extends Ingredient {
 		for (Map.Entry<Ingredient, Integer> entry : p.ingredientCounts.entrySet()) {
 			if (entry.getKey() instanceof Product) {
 				prettyPrintProduct((Product) entry.getKey(), prefix + "\t");
+				System.out.println(prefix + "\t" + "* " + entry.getValue());
 			} else {
-				System.out.println(prefix + "\t" + String.format("%s => %d", entry.getKey(), entry.getValue()));
+				System.out.println(prefix + "\t" + String.format("%s * %d", entry.getKey(), entry.getValue()));
 			}
 		}
 	}
