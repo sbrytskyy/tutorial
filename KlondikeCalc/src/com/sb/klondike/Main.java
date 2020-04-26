fbac91389b1c8f8960b2c1724565072ce1af76fd
Serhiy Brytskyy <sbrytskyy@gmail.com>
2020-04-26 12:39:28 -0700

    Main class cosmetic

--- KlondikeCalc/src/com/sb/klondike/Main.java
+++ KlondikeCalc/src/com/sb/klondike/Main.java
@@ -2,11 +2,7 @@ package com.sb.klondike;
 
 import com.sb.klondike.factory.Barn;
 import com.sb.klondike.factory.Smithy;
-import com.sb.klondike.product.Chains;
-import com.sb.klondike.product.Nails;
-import com.sb.klondike.product.Pipes;
 import com.sb.klondike.product.Product;
-import com.sb.klondike.product.Water;
 
 public class Main {
 
@@ -16,26 +12,16 @@ public class Main {
 	}
 
 	private void run() {
-		Water p1 = Barn.water();
-		Product.prettyPrintProduct(p1);
-		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p1));
+		printProduct(Barn.water());
+		printProduct(Smithy.nails());
+		printProduct(Smithy.pipes());
+		printProduct(Smithy.chains());
+	}
 
+	private void printProduct(Product p) {
 		System.out.println();
-
-		Nails p2 = Smithy.nails();
-		Product.prettyPrintProduct(p2);
-		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p2));
-
+		Product.prettyPrintProduct(p);
+		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p));
 		System.out.println();
-
-		Pipes p3 = Smithy.pipes();
-		Product.prettyPrintProduct(p3);
-		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p3));
-
-		System.out.println();
-
-		Chains p4 = Smithy.chains();
-		Product.prettyPrintProduct(p4);
-		System.out.println("Basic ingredients: " + Product.getTotalIngredientCounts(p4));
 	}
 }
