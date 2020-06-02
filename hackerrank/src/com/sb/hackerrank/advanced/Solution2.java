b75ca693803a60c5a1f4bcad6255a5650e34e04c
Serhiy Brytskyy <sbrytskyy@gmail.com>
2020-06-02 12:49:07 -0700

    Reflection tests

--- hackerrank/src/com/sb/hackerrank/advanced/Solution2.java
+++ hackerrank/src/com/sb/hackerrank/advanced/Solution2.java
@@ -1,13 +1,28 @@
 package com.sb.hackerrank.advanced;
 
+import java.lang.reflect.Constructor;
 import java.security.Permission;
 
-public class SolutionDoNotTerminate {
+public class Solution2 {
 
 	public static void main(String[] args) throws Exception {
 		DoNotTerminate.forbidExit();
 
 		try {
+			int num = 7;
+
+			Object o;// Must be used to hold the reference of the instance of the class
+						// Solution.Inner.Private
+
+			Class<?> innerClass = Inner.class.getDeclaredClasses()[0];
+			Constructor<?> ctor = innerClass.getDeclaredConstructors()[0];
+			Inner inner = Inner.class.newInstance();
+			ctor.setAccessible(true);
+			Inner.Private p = (Inner.Private) ctor.newInstance(inner);
+			o = p;
+			System.out.println(String.format("%d is %s", num, p.powerof2(num)));
+
+			System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
 
 		} // end of try
 
@@ -15,6 +30,14 @@ public class SolutionDoNotTerminate {
 			System.out.println("Unsuccessful Termination!!");
 		}
 	}// end of main
+
+	static class Inner {
+		private class Private {
+			private String powerof2(int num) {
+				return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
+			}
+		}
+	}// end of Inner
 
 	private static class DoNotTerminate { // This class prevents exit(0)
 
