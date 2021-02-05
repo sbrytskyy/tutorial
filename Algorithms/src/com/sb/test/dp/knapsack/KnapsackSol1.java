fd263ecea28a49af27499a70bdabc39d7c5e6c9e
Serhiy Brytskyy <sbrytskyy@gmail.com>
2021-02-04 18:09:24 -0800

    Knapsack implementation

---
+++ Algorithms/src/com/sb/test/dp/knapsack/KnapsackSol1.java
@@ -0,0 +1,66 @@
+package com.sb.test.dp.knapsack;
+
+import java.util.Arrays;
+
+import com.sb.lc.common.Utils;
+
+public class KnapsackSol1 {
+
+	private int[][] dp;
+
+	/**
+	 * @param items  Array of pairs [weight, value]
+	 * @param weight Max weight
+	 * @return Max value
+	 */
+	private int findMax(int[][] items, int weight) {
+		dp = new int[items.length + 1][weight + 1];
+
+		for (int i = 0; i < items.length; i++) {
+			dp[i][weight] = items[i][0];
+		}
+
+		int maxValue = 0;
+		int w = weight;
+
+		for (int i = 0; i < items.length; i++) {
+			int dpInd = i + 1;
+
+			int itemWeight = items[i][0];
+			int itemValue = items[i][1];
+
+			if (itemWeight > w) {
+				System.out.println("Item " + Arrays.toString(items[i]) + " not included as it's too heavy");
+				continue;
+			}
+
+			dp[dpInd][w] = dp[dpInd - 1][w];
+			dp[dpInd][w - itemWeight] = dp[dpInd - 1][w] + itemValue;
+
+			int max = 0;
+			if (dp[dpInd][w] > dp[dpInd][w - itemWeight]) {
+				max = dp[dpInd][w];
+				System.out.println("Item " + Arrays.toString(items[i]) + " not included");
+			} else {
+				max = dp[dpInd][w - itemWeight];
+				System.out.println("Item " + Arrays.toString(items[i]) + " included");
+				w -= itemWeight;
+			}
+			maxValue = Math.max(maxValue, max);
+		}
+
+		Utils.printArray2D(dp, "DP");
+
+		return maxValue;
+	}
+
+	public static void main(String[] args) {
+		KnapsackSol1 sol = new KnapsackSol1();
+
+		int[][] items = new int[][] { { 5, 10 }, { 7, 13 }, { 9, 19 }, { 2, 4 } };
+		int weight = 10;
+		int maxVal = sol.findMax(items, weight);
+		System.out.println("Max value: " + maxVal);
+
+	}
+}
