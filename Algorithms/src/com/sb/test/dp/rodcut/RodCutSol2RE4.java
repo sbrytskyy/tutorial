f1cd7cc6d2f87bae7ae19ec092278529efef1493
Serhiy Brytskyy <sbrytskyy@gmail.com>
2021-02-08 22:29:28 -0800

    Added comments

--- Algorithms/src/com/sb/test/dp/rodcut/RodCutSol2RE4.java
+++ Algorithms/src/com/sb/test/dp/rodcut/RodCutSol2RE4.java
@@ -4,18 +4,28 @@ public class RodCutSol2RE4 {
 
 	public int findMaxValue(int[] prices, int length) {
 
+		// store for best prices for different lengths from 0 to length
+		// if length is 0, value is 0
 		int[] dp = new int[length + 1];
 
 		for (int l = 1; l <= length; l++) {
+			// we find max value for rod of length L starting from 1 to length
 			int max = 0;
 
 			for (int m = 1; m <= l; m++) {
-				int value = prices[m - 1] + dp[l - m];
+
+				// cut rod of length L into two different combinations
+				// value equals to price of piece of length M and best price of length L - M,
+				// which is already calculated.
+
+				int value = prices[/* price index= */ m - 1] + dp[l - m];
 				if (value > max) {
+					// found new better value
 					max = value;
 				}
 			}
 
+			// store max value for length L
 			dp[l] = max;
 		}
 
